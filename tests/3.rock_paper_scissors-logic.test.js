d59dfdca2ffc098c1a66a729a78ba8bc8a75165b
Jiymeade <jiymeade@gmail.com>
2024-03-24 15:00:26 -0400

    RPS TEST Passed

---
+++ tests/3.rock_paper_scissors-logic.test.js
@@ -0,0 +1,27 @@
+const { RockPaperScissors } = require(`../resources/scripts/rock_paper_scissors.js`);
+
+describe(`RockPaperScissors class`, function () {
+  describe(`determineWinner()`, function () {
+    test(`win cases`, function () {
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `scissors`)).toBe(`win`);
+      // Complete the test
+    });
+
+    test(`tie cases`, function () {
+      // Write your test here
+      const game =  new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `rock`)).toBe(`tie`);
+      expect(game.determineWinner(`paper`, `paper`)).toBe(`tie`);
+      expect(game.determineWinner(`scissors`, `scissors`)).toBe(`tie`);
+    });
+
+    test(`lost cases`, function () {
+      // Write your test here
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `paper`)).toBe(`lose`);
+      expect(game.determineWinner(`paper`, `scissors`)).toBe(`lose`);
+      expect(game.determineWinner(`scissors`, `rock`)).toBe(`lose`);
+    });
+  });
+});
